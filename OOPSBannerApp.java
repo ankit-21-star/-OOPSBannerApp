a048ef5dd3b506b3aa74ea050aa5ead212a7f7f9
ankit-21-star <aa9976@srmist.edu.in>
2026-02-16 19:38:00 +0530

    Initial commit -UC1 Print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This application prints "OOPS" (Object-Oriented Programming System)
+ * to the console.
+ * 
+ * @author Ankit Kumar Ghosh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String args[]) {
+        System.out.println("OOPS");
+    }
+}

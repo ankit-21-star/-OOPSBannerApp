beb027faf1592f4a79480be2898113bf795e3481
ankit-21-star <aa9976@srmist.edu.in>
2026-03-08 02:37:44 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This program prints the text "OOPS" to the console.
+ * It demonstrates the basic structure of a Java program.
+ *
+ * @author Ankit
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Print OOPS to the console
+        System.out.println("OOPS");
+
+    }
+}

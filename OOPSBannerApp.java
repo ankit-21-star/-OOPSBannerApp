51767f7891d8b6f6d120aebe7222c55f9ed6c5c0
ankit-21-star <aa9976@srmist.edu.in>
2026-03-08 17:37:15 +0530

    Refactored UC6 using helper methods for character patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,70 @@
+/**
+ * OOPSBannerApp
+ *
+ * Prints OOPS banner using helper methods
+ * for each character pattern.
+ *
+ * @author Ankit
+ * @version 1.0
+ */
+
 public class OOPSBannerApp {
+
+    // Method for letter O
+    public static String[] getOPattern() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Method for letter P
+    public static String[] getPPattern() {
+        return new String[] {
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Method for letter S
+    public static String[] getSPattern() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*      ",
+            " ***** ",
+            "      *",
+            "*     *",
+            " ***** "
+        };
+    }
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join(" ", " ***** ", " ***** ", " ****** ", " ***** "),
-            String.join(" ", "*     *", "*     *", "*     * ", "*     *"),
-            String.join(" ", "*     *", "*     *", "*     * ", "*"),
-            String.join(" ", "*     *", "*     *", "****** ", " ***** "),
-            String.join(" ", "*     *", "*     *", "*      ", "      *"),
-            String.join(" ", "*     *", "*     *", "*      ", "*     *"),
-            String.join(" ", " ***** ", " ***** ", "*      ", " ***** ")
-        };
+        String[] O = getOPattern();
+        String[] P = getPPattern();
+        String[] S = getSPattern();
 
-        for (String line : banner) {
-            System.out.println(line);
+        // Print banner using loop
+        for (int i = 0; i < 7; i++) {
+            System.out.println(
+                String.join(" ",
+                    O[i],   // First O
+                    O[i],   // Second O
+                    P[i],   // P
+                    S[i]    // S
+                )
+            );
         }
-
-    }   // closes main method
-
-}       // closes class
+    }
+}
